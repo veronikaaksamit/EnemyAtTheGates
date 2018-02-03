e2c0cdc2c2448c1d124fa4adcea172d7b32a61ca
veronikaaksamit <veronika.aksamitova@gmail.com>
2018-02-03 13:06:54 +0100

    changing numbers of mines and so on

---
+++ Assets/UtilityNumbeUpdater.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UtilityNumbeUpdater : MonoBehaviour
+{
+
+    public Player MyPlayer;
+
+    private Text[] texts;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    texts = GetComponentsInChildren<Text>(true);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    UpdateTexts();
+	}
+
+    private void UpdateTexts()
+    {
+        for (int i = 0; i < texts.Length; i++)
+        {
+            switch (texts[i].tag)
+            {
+                case "BarbedWire":
+                    texts[i].text = MyPlayer.NumOfWires.ToString();
+                    break;
+                case "Mine":
+                    texts[i].text = MyPlayer.NumOfMines.ToString();
+                    break;
+                case "TankBarrier":
+                    texts[i].text = MyPlayer.NumOfTankBarriers.ToString();
+                    break;
+                case "Gold": break;
+                default:
+                    Debug.Log(texts[i].tag + "is not between cases");
+                    break;
+            }
+        }
+    }
+}
