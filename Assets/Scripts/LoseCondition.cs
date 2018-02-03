cb946fb1fdfd9f948529b08e2f8c51f4b13a242a
DarebacK <dareback1@gmail.com>
2018-02-03 15:05:56 +0100

    lose condition

---
+++ Assets/Scripts/LoseCondition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class LoseCondition : MonoBehaviour
+{
+    public UnityEvent GameLost;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<IEnemyUnit>() == null)
+        {
+            return;
+        }
+
+        if (GameLost != null)
+        {
+            GameLost.Invoke();
+        }
+    }
+}
