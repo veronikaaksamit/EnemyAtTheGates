cc34bba24ed766dc8d61d5e56b6ac3b9302ec5b2
veronikaaksamit <veronika.aksamitova@gmail.com>
2018-02-04 20:40:08 +0100

    added buttons for new game when lost or won game

---
+++ Assets/NewGameLoading.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameLoading : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadNewGame ()
+	{
+	    Application.LoadLevel("Level1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
