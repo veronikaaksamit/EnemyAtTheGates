4ae10a2078bafbb6077e7a8e0b361c235650a6da
veronikaaksamit <veronika.aksamitova@gmail.com>
2018-02-04 20:07:18 +0100

    added description of buttons

---
+++ Assets/ChangeButtonDescription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeButtonDescription : MonoBehaviour
+{
+	
+    public void AddText(String text)
+    {
+        this.GetComponent<Text>().text = text;
+        Debug.Log("hovering");
+    }
+
+    public void RemoveText()
+    {
+        this.GetComponent<Text>().text = "";
+    }
+
+}
